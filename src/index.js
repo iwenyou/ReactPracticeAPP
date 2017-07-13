3b366c06c108efc9ea78af865a7992ace03461c7
Wen Zhang <iwenyou@gmail.com>
2017-07-13 16:34:40 -0700

    change index const to class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, {  Component  } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyBCRQqTfB6LwEUPd_Ak6oAb4n-6F_Nv9uc';
 
 YTSearch({key:API_KEY, term:'surfboards'}, function(data){
@@ -10,12 +9,14 @@ YTSearch({key:API_KEY, term:'surfboards'}, function(data){
 });
 
 //create a new component. This component should produce some html
-const App = () => {
-  return(
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 //Take this component generated Html and render it on the page
