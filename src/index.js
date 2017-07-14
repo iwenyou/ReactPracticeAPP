6d99a874ebf890e6e7a83845df2baa07824227d4
Wen Zhang <iwenyou@gmail.com>
2017-07-13 17:48:36 -0700

    added video list

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ import React, {  Component  } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyBCRQqTfB6LwEUPd_Ak6oAb4n-6F_Nv9uc';
-
-YTSearch({key:API_KEY, term:'surfboards'}, function(data){
-  console.log(data);
-});
 
 //create a new component. This component should produce some html
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key:API_KEY, term:'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
   render() {
     return(
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
