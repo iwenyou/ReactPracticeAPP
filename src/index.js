a4a2a0f739c085e70a0ce2106a825e7838a6eb01
Wen Zhang <iwenyou@gmail.com>
2017-07-16 13:34:49 -0700

    added selectedVideo

--- src/index.js
+++ src/index.js
@@ -1,25 +1,36 @@
-import React, {  Component  } from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyBCRQqTfB6LwEUPd_Ak6oAb4n-6F_Nv9uc';
 
 //create a new component. This component should produce some html
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({key:API_KEY, term:'surfboards'}, (videos) => {
-      this.setState({ videos });
+    YTSearch({
+      key: API_KEY,
+      term: 'surfboards'
+    }, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       });
     });
   }
   render() {
-    return(
+    return (
       <div>
-        <SearchBar />
+        <SearchBar/>
+        <VideoDetail video={this.state.selectedVideo}/>
         <VideoList videos={this.state.videos}/>
       </div>
     );
@@ -28,4 +39,5 @@ class App extends Component {
 
 //Take this component generated Html and render it on the page
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+  <App/>, document.querySelector('.container'));
